68d3f84feb0f00ac27568220ffef7e9f8d234bda
José Luis Zúñiga <jose@tangibledesign.co>
2024-08-13 16:21:52 -0500

    Add prube connection in main

--- src/main/java/org/jlz/Main.java
+++ src/main/java/org/jlz/Main.java
@@ -5,19 +5,19 @@ import org.jlz.entity.Almacenes;
 import org.jlz.util.UtilEntity;
 
 import java.util.List;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
   public static void main(String[] args) {
-    // Obtiene un EntityManager para interactuar con la base de datos
-    EntityManager em = UtilEntity.getEntityManagerFactory();
-
-    List<Almacenes> almacenes = em.createQuery("SELECT a FROM Almacenes a", Almacenes.class).getResultList();
-    System.out.println("------- Listar Datos -------");
-    // Imprime cada usuario en la lista
-    almacenes.forEach(System.out::println);
-    System.out.println("----------------------------\n");
+    try (EntityManager em = UtilEntity.getEntityManagerFactory()) {
+      List<Almacenes> almacenes = em.createQuery("SELECT a FROM Almacenes a", Almacenes.class).getResultList();
+      System.out.println("------- Listar Datos -------");
+      almacenes.forEach(System.out::println);
+      System.out.println("----------------------------\n");
+    } catch (Exception e) {
+      e.printStackTrace();
+      // Manejo de excepciones específico
+    }
   }
 }
 
